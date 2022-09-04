ec13cb0783185fb0b8ecc7c18aa8bd3718594bc1
Vkscoma <vkscoma@gmail.com>
2022-09-04 14:03:09 -0700

    Added JS to form
    Added validation and JS function to capture form data.

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,3 +242,43 @@ navLinks.forEach((link) => {
     xIcon.style.display = "none";
   });
 });
+
+//Form JS
+const form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  let status = document.getElementById("form-status");
+  let data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        status.classList.add("success-msg-color");
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.classList.add("error-msg-color");
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+            status.classList.add("error-msg-color");
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.className.add("error-msg-color");
+    });
+}
+form.addEventListener("submit", handleSubmit);
